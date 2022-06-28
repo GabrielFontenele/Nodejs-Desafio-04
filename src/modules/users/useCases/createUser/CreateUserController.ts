9a45a2537cbdd7dac66c00418222033568823deb
Gabriel <gabrielrad2@gmail.com>
2022-06-27 21:09:38 -0300

    final

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,18 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { email, name } = request.body;
+      const user = this.createUserUseCase.execute({ email, name });
+
+      return response.status(201).json({
+        name: user.name,
+        email: user.email,
+        admin: user.admin,
+      });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const user_id = Array.isArray(request.headers.user_id)
+        ? request.headers.user_id[0]
+        : request.headers.user_id;
+
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: err });
+    }
   }
 }
 
